a675424d25d98492c4714ba198553c00d3f30df9
Matis Basso <91749838+Exyo02@users.noreply.github.com>
2025-05-13 14:54:20 +0200

    Modifications de la barre de navigation + fonction actus et aménagement deviennent infos et contacts

--- frontend/src/app/components/infos/infos.component.spec.ts
+++ frontend/src/app/components/infos/infos.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ActusComponent } from './actus.component';
+import { ActusComponent } from './infos.component';
 
 describe('ActusComponent', () => {
   let component: ActusComponent;
---
+++ frontend/src/app/components/infos/infos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-infos',
+  imports: [],
+  templateUrl: './infos.component.html',
+  styleUrl: './infos.component.css'
+})
+export class InfosComponent {
+
+}
